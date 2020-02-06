cdad606d73e5ce70793a069d75efc4468a2f3589
iulianpro <iulian.prodea@gmail.com>
2020-02-06 21:46:39 +0000

    Cleaned js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,24 @@
-var imgs = ['/assets/images/visit-1.jpg', '/assets/images/visit-2.jpg', '/assets/images/visit-3.jpg', '/assets/images/visit-4.jpg', '/assets/images/visit-5.jpg', '/assets/images/main-image.jpg'];
+let imgs = ['visit-1.jpg', 'visit-2.jpg', 'visit-3.jpg', 'visit-4.jpg', 'visit-5.jpg', 'main-image.jpg'];
 
-$('.callout-img').fadeIn(350, mySlide());
+$('.callout-img').fadeIn(0, changeSlider());
 
-function mySlide() {
+function changeSlider() {
     setTimeout(function () {
-        var url = imgs[imgs.push(imgs.shift()) - 1];
-        $('.callout-img').delay(2000).fadeOut(300, function () {
-            $(this).css('background-image', 'url(' + url + ')')
+        let path = imgs[imgs.push(imgs.shift()) - 1];
+        $('.callout-img').delay(2000).fadeOut(350, function () {
+            $(this).css('background-image', 'url(/assets/images/' + path + ')')
             $(this).css('background-repeat', 'no-repeat')
             $(this).css('background-position', 'center center')
-        }).fadeIn(350, mySlide())
+        }).fadeIn(350, changeSlider())
     });
 }
 
-$('.card-area').mouseenter(function () {
-    $(this).children('div.overlay-d-none').css({ 'display': 'flex' })
-});
+$('.card-area').hover(function () {
+    $(this).children('.overlay-d-none').css({ 'display': 'flex' })    // over
+    $(this).children('.card-body').css({ 'color': '#c0f8ea' })
 
-$('.card-area').mouseleave(function () {
-    $('.overlay-d-none').css({ 'display': 'none' })
-});
-
-$('.card-area').mouseenter(function () {
-    $(this).children('div.card-body').css({ 'color': '#c0f8ea' })
-});
-
-$('.card-area').mouseleave(function () {
+}, function () {
+    $('.overlay-d-none').css({ 'display': 'none' })    // out
     $('.card-body').css({ 'color': '#4d4d4d' })
-});
+}
+);
